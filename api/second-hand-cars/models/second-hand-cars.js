970956b878be1dccc37b132951a2ff1a7b3c6c7f
florian.J <62655839+Hydrog3n69@users.noreply.github.com>
2021-07-19 09:33:56 +0200

    add entries

--- api/second-hand-cars/models/second-hand-cars.js
+++ api/second-hand-cars/models/second-hand-cars.js
@@ -5,4 +5,14 @@
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+     settings: {
+    parser: {
+      enabled: true,
+      multipart: true,
+      formidable: {
+        maxFileSize: 200 * 1024 * 1024 // Defaults to 200mb
+      }
+    }
+  },
+};
